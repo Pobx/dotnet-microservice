9be1d1e08dd18a56dec425cca5b31a090740a36d
Pobx <geidtiphong.s@allrightbox.com>
2022-03-06 14:28:42 +0700

    add AutoMapper to startup

--- Startup.cs
+++ Startup.cs
@@ -14,52 +14,45 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PlatformService.Data;
 
-namespace PlatformService
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+namespace PlatformService {
+	public class Startup {
+		public Startup (IConfiguration configuration) {
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices (IServiceCollection services) {
 
-            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
-            services.AddScoped<IPlatformRepository, PlatformRepository>();
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
-            });
-        }
+			services.AddDbContext<AppDbContext> (opt => opt.UseInMemoryDatabase ("InMem"));
+			services.AddScoped<IPlatformRepository, PlatformRepository> ();
+			services.AddAutoMapper (AppDomain.CurrentDomain.GetAssemblies ());
+			services.AddControllers ();
+			services.AddSwaggerGen (c => {
+				c.SwaggerDoc ("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
+			});
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+			if (env.IsDevelopment ()) {
+				app.UseDeveloperExceptionPage ();
+				app.UseSwagger ();
+				app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "PlatformService v1"));
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection ();
 
-            app.UseRouting();
+			app.UseRouting ();
 
-            app.UseAuthorization();
+			app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+			app.UseEndpoints (endpoints => {
+				endpoints.MapControllers ();
+			});
 
-			PrepDb.PrepPopulation(app);
-        }
-    }
+			PrepDb.PrepPopulation (app);
+		}
+	}
 }
